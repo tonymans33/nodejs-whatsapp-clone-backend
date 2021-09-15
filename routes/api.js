205f011de1f625a5e3e710317a2e94eef2394316
tonymans33 <tonymansour66@yahoo.com>
2021-09-16 01:47:30 +0200

    changing swagger api endpoint

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const chatRoutes = require("./chatRoutes");
 const messageRoutes = require("./messageRoutes");
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger/swagger.json');
+const swaggerDocument = require('../swagger/swagger.js');
 
 var app = express();
 
@@ -10,8 +10,8 @@ app.use("/chats", chatRoutes);
 app.use("/messages", messageRoutes);
 
 // Swagger route 
-app.use('/api-docs', swaggerUi.serve);
-app.get('/api-docs', swaggerUi.setup(swaggerDocument));
+app.use('/v1/docs', swaggerUi.serve);
+app.get('/v1/docs', swaggerUi.setup(swaggerDocument));
 
 app.get('/test', (req, res) => res.status(200).send("Test API is working!!!"));
 
